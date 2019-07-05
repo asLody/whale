45662a19d4109d6b753efef33a3879aa459a89cc
fettdrac <30793510+fettdrac@users.noreply.github.com>
2019-07-05 18:03:23 +0800

    Update ContextUtil.java

--- java/com/hmtest/ContextUtil.java
+++ java/com/hmtest/ContextUtil.java
@@ -1,4 +1,4 @@
-package com.pvdnc.hmtest;
+package com.yourpackagename.hmtest;
 
 import android.app.Application;
 import android.content.Context;
@@ -8,6 +8,7 @@ import java.lang.reflect.Method;
 public class ContextUtil {
     public static Context getAppContext(){
         try {
+            //It can be ensured that when module calls this,hidden API policy has been disabled.
             Class<?> activityThread=Class.forName("android.app.ActivityThread");
             Method currentApplication=activityThread.getDeclaredMethod("currentApplication");
 
